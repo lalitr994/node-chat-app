7bcb58280b2b1826221a24dd6609fd141563c7b5
lalitr994 <lalit.rajput@jadeglobal.com>
2018-04-20 17:45:53 +0530

    admin msgs

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
-const publicPath = path.join(__dirname,'../public');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
@@ -11,32 +11,48 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('New user connected');
-   
-    socket.on('createMessage', (msg)=>{
-        console.log('createMessage', msg);
-        io.emit('newMessage',{
-            from : msg.from,
-            text : msg.text,
-            createdAt : new Date().getTime()
-        })
-    })
 
-    socket.on('disconnect',()=>{
+    
+    socket.emit('newMessage',{
+        from : "Admin",
+        text : "Welcome to the chat app",
+        createdAt : new Date().getTime()
+    });
+
+    // socket.broadcast.emit from admin text new user
+    socket.broadcast.emit('newMessage',{
+        from : 'Admin',
+        text : 'New user joined',
+        createdAt : new Date().getTime()
+    });
+
+
+    // socket.on('createMessage', (msg) => {
+    //     console.log('createMessage', msg);
+
+    //     // socket.broadcast.emit('newMessage', {
+    //     //     from: msg.from,
+    //     //     text: msg.text,
+    //     //     createdAt: new Date().getTime()
+    //     // })
+    // })
+
+    socket.on('disconnect', () => {
         console.log('User was disconnected');
     })
 
 
-    
 
- 
+
+
 })
 
 
 
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log(`***Server running at ${port}***`)
 })
 
